a8327cc2f3f40ad4414e990271736f726d313381
Konstantin Astafurov <40547702+konstantin-aa@users.noreply.github.com>
2022-09-01 17:54:40 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python-downloader
-a small cli to install mp4/mp3 files from youtube
+a small cli I wrote to download some songs for my brother. I found out about `youtube-dl` once I was 80% done with the script, lol.  
+uses ffmpeg to reencode mp3 files. Didn't test with mp4 yet. Will add a playlist option as well.
+# Usage: 
+`test.py -l <yt-link> -o <output-path | output-dir if reading from a file> -f <txt-file>`
+
+# txt-file format:
+On each line:
+youtube-link | relative-path.extension
+trailing newlines will error!
---
+++ main.py
@@ -0,0 +1,42 @@
+# importing the module 
+from pytube import YouTube
+import sys, getopt
+import os
+# where to save 
+SAVE_PATH = "E:/" #to_do 
+
+# link of the video to be downloaded 
+link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
+
+def main(argv):
+    opts = []
+    try:
+        opts, args = getopt.getopt(argv,"hl:o:f:")
+    except getopt.GetoptError:
+        sys.exit(2)
+    opts_dict = dict(opts)
+    jobs = []
+    out = None
+    if "-f" in opts_dict:
+        out = opts_dict["-o"]
+        with open(opts_dict["-f"], "r") as f:
+            for line in f.readlines():
+                temp = line.split(" | ")
+                jobs += [(temp[0], temp[1].split("\n")[0])] # newline
+    else:
+        jobs = list(zip(map(lambda opt: opt[1], filter(lambda opt: opt[0] == "-l", opts)), map(lambda opt: opt[1], filter(lambda opt: opt[0] == "-o", opts))))
+    for link, output_file in jobs:
+        print(f"downloading link {link} as {output_file}")
+        try:
+            # object creation using YouTube
+            # which was imported in the beginning 
+            yt = YouTube(link)
+        except:
+            raise(Exception("Connection Error"))
+        ext = output_file.split(".")[-1]
+        streams = yt.streams.filter(only_audio=True) if ext == "mp3" else yt.streams.filter(file_extension=ext)
+        streams[0].download(output_path=out, filename=(output_file if ext != "mp3" else "temp.wav"))
+        if ext == "mp3":
+            os.system(f"ffmpeg -i {((out + '/') if out else '') + 'temp.wav'} -codec:a libmp3lame -qscale:a 2 " + ((out + "/") if out else "") + '\ '.join(output_file.split(' ')))
+if __name__ == "__main__":
+   main(sys.argv[1:])
